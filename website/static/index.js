93801c6ae2283f977d602f6ef5ed0a30d21e2bde
DaMrCSR <125293604+DaMrCSR@users.noreply.github.com>
2023-02-13 11:22:19 -0500

    Add files via upload

---
+++ website/static/index.js
@@ -0,0 +1,22 @@
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
+  
+  function hideX() {
+    var x = document.getElementById("deleteX");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+function openModal() {
+  $("#areyousureModal").modal()
+}
+
