fda443afaf3754e0a139b66848a77bca59e67fc5
DaMrCSR <125293604+DaMrCSR@users.noreply.github.com>
2023-02-27 23:48:07 -0500

    Update index.js

--- website/static/index.js
+++ website/static/index.js
@@ -7,8 +7,8 @@ function deleteNote(noteId) {
     });
   }
   
-  function hideX() {
-    var x = document.getElementById("deleteX");
+  function hideX(btn) {
+    var x = btn.previousElementSibling;
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
@@ -19,4 +19,3 @@ function deleteNote(noteId) {
 function openModal() {
   $("#areyousureModal").modal()
 }
-
